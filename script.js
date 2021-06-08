eae1a3c41f8dae60b0b2788ec42703342a934131
jamboooooo <hisllam35@gmail.com>
2021-06-08 14:01:13 +0300

    release 3

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 const chalk = require('chalk');
+const boxen = require('boxen');
 
-console.log(chalk.bold.red.underline.bgGreen('Hello, world'))
+const yargs = require('yargs')
+
+
+//console.log(boxen(chalk.bold.yellow.bgGrey('Hello, world'), { borderColor: 'green', borderStyle: 'round' }))
+
+yargs.command({
+    command: 'login',
+    describe: 'Авторизоваться',
+    login: {
+        type: 'string',
+        demandOption: true,
+        describe: 'Логин'
+    },
+    password: {
+        type: 'string',
+        demandOption: true,
+        describe: 'Пароль'
+    },
+    handler(login) {
+        if (login.login === 'admin' && login.password === 'qwer') {
+            console.log('вы авторизованы');
+        } else {
+            console.log('Ошибка авторизации');
+        }
+    }
+})
+
+yargs.parse()
